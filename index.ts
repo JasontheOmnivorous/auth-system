44a2d0a6d55a10a3d48c791bfd6de770f190b14c
Min Thant <126573394+JasontheOmnivorous@users.noreply.github.com>
2023-11-27 17:22:22 +0630

    handle undefined routes

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { Request, Response, NextFunction } from "express";
 import userRouter from "./route/userRoute";
 import { globalErrorHandler } from "./utils/globalErrorHandler";
 const app = express();
 
 app.use(userRouter);
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new AppError("This route is not defined.", 404);
+});
 app.use(globalErrorHandler);
 
 export default app;
