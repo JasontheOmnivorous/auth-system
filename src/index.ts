b0bce693dbeb143222ffedac82b0aad6c181cf07
JasontheOmnivorous <marsjason485@gmail.com>
2024-01-18 13:59:23 +0630

    placeholder response

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.get("/", (req: Request, res: Response, next: NextFunction) =>
+  res.send(
+    "What's up bro! Start using the API by signing in with your account. Check api documentation in my github repository for more details."
+  )
+);
 app.use("/api/v1/users", userRouter);
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
   next(new AppError("This route is not defined.", 404));
